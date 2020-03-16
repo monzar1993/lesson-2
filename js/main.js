98f44aad8f0cf2b62ac24c5b445d513ac1c423fe
Игорь Тихоненко <monzar1993@outlook.com>
2020-03-16 14:49:48 +0200

    task-2 main+additional

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-"use strict";!function u(i,f,c){function a(e,r){if(!f[e]){if(!i[e]){var n="function"==typeof require&&require;if(!r&&n)return n(e,!0);if(p)return p(e,!0);var t=new Error("Cannot find module '"+e+"'");throw t.code="MODULE_NOT_FOUND",t}var o=f[e]={exports:{}};i[e][0].call(o.exports,function(r){return a(i[e][1][r]||r)},o,o.exports,u,i,f,c)}return f[e].exports}for(var p="function"==typeof require&&require,r=0;r<c.length;r++)a(c[r]);return a}({1:[function(r,e,n){},{}]},{},[1]);
+//Lesson-2
+
+//Task 1 start 
+
+var letters = []
+var sentence = 'Backend As A Service'
+var arr = sentence.split(' ')
+console.log(arr)
+for (var i = 0; i < arr.length; i++) {
+    letters.push(arr[i][0])
+}
+console.log(letters)
+console.log(letters.join(' '))
+
+//Task 1 end
+
+//Task 2 Additional
+var now = new Date()
+
+function check(a) {
+    if (typeof a === 'number') {
+        console.log(now.toLocaleString())
+    } else {
+        console.log("Неверный тип данных")
+    }
+}
+check(4)
+
+//Task 2 Additional
